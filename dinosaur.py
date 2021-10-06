045c8d17f77eb4dec25152fd57aa105ad7744af2
JESSE ARROYO-ROMERO <jesseromero310@gmail.com>
2021-10-06 10:43:25 -0500

    Finished creating the classes and laying down the blueprints.

---
+++ dinosaur.py
@@ -0,0 +1,9 @@
+class Dinosaur:
+    def __init__(self):
+        self.name = ''
+        self.attack_power = 100
+        self.health = 2000
+        
+
+    def attack(self, robot):
+        pass
---
+++ robot.py
@@ -0,0 +1,9 @@
+class Robot:
+    def __init__(self, weapon):
+        self.name = ''
+        self.health = 1500
+        self.weapon = weapon
+        
+
+    def attack(self, dinosaur):
+        pass
